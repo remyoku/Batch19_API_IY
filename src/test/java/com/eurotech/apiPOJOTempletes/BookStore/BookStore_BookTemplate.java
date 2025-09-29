8ea53470ec99346203fc534e5b17f3d2acdc1f08
İrem <iremyokus2113@gmail.com>
2025-09-29 19:39:17 +0300

    API Tests With Pojo

---
+++ src/test/java/com/eurotech/apiPOJOTempletes/BookStore/BookStore_BookTemplate.java
@@ -0,0 +1,118 @@
+package com.eurotech.apiPOJOTempletes.BookStore;
+
+import java.util.Date;
+
+public class BookStore_BookTemplate {
+
+    private String isbn;
+    private String title;
+    private String subTitle;
+    private String author;
+    private Date publish_date;
+    private String publisher;
+    private int pages;
+    private String description;
+    private String website;
+
+    public BookStore_BookTemplate() {
+    }
+
+    public BookStore_BookTemplate(String isbn, String title, String subTitle, String author, Date publish_date, String publisher, int pages, String description, String website) {
+        this.isbn = isbn;
+        this.title = title;
+        this.subTitle = subTitle;
+        this.author = author;
+        this.publish_date = publish_date;
+        this.publisher = publisher;
+        this.pages = pages;
+        this.description = description;
+        this.website = website;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Date getPublish_date() {
+        return publish_date;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublish_date(Date publish_date) {
+        this.publish_date = publish_date;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public String toString() {
+        return "BookStore_BookTemplate{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", subTitle='" + subTitle + '\'' +
+                ", author='" + author + '\'' +
+                ", publish_date=" + publish_date +
+                ", publisher='" + publisher + '\'' +
+                ", pages=" + pages +
+                ", description='" + description + '\'' +
+                ", website='" + website + '\'' +
+                '}';
+    }
+}
--- src/test/java/com/eurotech/apiTests/day_05/C02_BookStore_POJO_Task.java
+++ src/test/java/com/eurotech/apiTests/day_05/C02_BookStore_POJO_Task.java
@@ -1,5 +1,10 @@
 package com.eurotech.apiTests.day_05;
 
+import com.eurotech.apiPOJOTempletes.BookStore.BookStore_BookTemplate;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class C02_BookStore_POJO_Task {
@@ -20,5 +25,35 @@ public class C02_BookStore_POJO_Task {
          * verify that publisher is "O'Reilly Media"
          * verify that pages is 460
          */
+
+        String baseUrl = "https://bookstore.toolsqa.com";
+
+        Response response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .queryParam("isbn", "9781449365035")
+                .when()
+                .get(baseUrl + "/BookStore/v1/Book");
+
+        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.contentType(),"application/json; charset=utf-8");
+
+        response.prettyPrint();
+
+        BookStore_BookTemplate bookStore = response.as(BookStore_BookTemplate.class);
+
+        String isbn = bookStore.getIsbn();
+        Assert.assertEquals(isbn,"9781449365035");
+
+        String title = bookStore.getTitle();
+        Assert.assertEquals(title,"Speaking JavaScript");
+
+        String author = bookStore.getAuthor();
+        Assert.assertEquals(author,"Axel Rauschmayer");
+
+        String publisher = bookStore.getPublisher();
+        Assert.assertEquals(publisher,"O'Reilly Media");
+
+        int pages = bookStore.getPages();
+        Assert.assertEquals(pages,460);
     }
 }
